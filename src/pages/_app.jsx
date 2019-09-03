9e65694c7ea5c922833b5242e9a6afa444e9fcbd
Daniël Botha <botha.daniel1@gmail.com>
2019-09-03 12:50:52 +0200

    Remove deprecated container

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import App, { Container } from 'next/app';
+import App from 'next/app';
 import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -37,7 +37,7 @@ class MyApp extends App {
     const { Component, pageProps } = this.props;
 
     return (
-      <Container>
+      <>
         <Head>
           <title>My page</title>
         </Head>
@@ -46,7 +46,7 @@ class MyApp extends App {
           <CssBaseline />
           <Component {...pageProps} />
         </ThemeProvider>
-      </Container>
+      </>
     );
   }
 }
