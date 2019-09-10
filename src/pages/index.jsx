1a8a44fb41bf5d28a431aa73bf2e667a43b8a5d3
Daniël Botha <botha.daniel1@gmail.com>
2019-09-10 14:57:49 +0200

    Wrap index page around InstanceProvider

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,18 @@ import Container from '@material-ui/core/Container';
 import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 
+import { InstanceProvider } from '../context/InstanceContext';
 import MyTabs from '../components/MyTabs';
 
 const Padding = styled(Typography)({ padding: '2rem 0' });
 
 export default function Index() {
   return (
-    <Container maxWidth="lg">
-      <Padding variant="h3">Amazon EC2 launch wizard</Padding>
-      <MyTabs />
-    </Container>
+    <InstanceProvider>
+      <Container maxWidth="lg">
+        <Padding variant="h3">Amazon EC2 launch wizard</Padding>
+        <MyTabs />
+      </Container>
+    </InstanceProvider>
   );
 }
