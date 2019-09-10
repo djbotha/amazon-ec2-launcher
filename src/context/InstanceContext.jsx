beb36f886a2b007320b109ec3a016e223247f008
Daniël Botha <botha.daniel1@gmail.com>
2019-09-10 15:03:38 +0200

    Add JSDoc comments to InstanceContext

--- src/context/InstanceContext.jsx
+++ src/context/InstanceContext.jsx
@@ -2,6 +2,12 @@ import React, { createContext, useContext, useReducer } from 'react';
 
 const InstanceContext = createContext();
 
+/**
+ * Reducer function used to update our state
+ *
+ * @param {object} state our current state
+ * @param {object} action contains 1. the type of action to execute, and 2. the payload to add to state.
+ */
 function reducer(state, action) {
   switch (action.type) {
     case 'AMI': {
@@ -18,11 +24,19 @@ function reducer(state, action) {
   }
 }
 
+/**
+ * Middelware for InstanceContext.Provider in order to inject reducer state and dispatch
+ *
+ * @param {object} props Additional props that are passed to this funciton
+ */
 function InstanceProvider(props) {
   const [state, dispatch] = useReducer(reducer, {});
   return <InstanceContext.Provider value={{ state, dispatch }} {...props} />;
 }
 
+/**
+ * Helper Hook to check if our context is null. This makes it easier to call `useContext` where we need it.
+ */
 function useInstance() {
   const context = useContext(InstanceContext);
   if (context === undefined) {
