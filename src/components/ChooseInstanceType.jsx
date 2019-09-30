144ae12e4b81b1c6d6936e4c02369536d5784e48
Daniël Botha <botha.daniel1@gmail.com>
2019-09-30 13:12:01 +0200

    Resolved issues with filters

--- src/components/ChooseInstanceType.jsx
+++ src/components/ChooseInstanceType.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Box from '@material-ui/core/Box';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -10,53 +10,57 @@ import Switch from '@material-ui/core/Switch';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 
-import INSTANCES from "../static/instance"
+import INSTANCES from '../static/instance';
 
 export default function ChooseInstanceType() {
-  const [instances, setInstances] = useState(INSTANCES)
+  const [instances, setInstances] = useState(INSTANCES);
   const [filters, setFilters] = useState({
     freeTier: false,
     turbo: false,
-    avx: false,
-    aesni: false,
+    AVX: false,
+    AESNI: false,
     IPv6: false,
-    EBSOptimized: false,
-    family: "All"
+    EBSOptimizedAvailable: false,
+    family: 'All'
   });
 
   const handleToggle = key => {
-    setFilters({ ...filters, [key]: !filters[key]});
-  }
+    setFilters({ ...filters, [key]: !filters[key] });
+  };
 
-  const handleFamily = (e) => {
-    setFilters({ ...filters, family: e.target.value});
-  }
+  const handleFamily = e => {
+    setFilters({ ...filters, family: e.target.value });
+  };
 
-  const filterFamily = (instance) => {
-    if (family === "All") {
-      return true
-    } 
-    if (family === instance.family) {
-      return true
-    }
-    return false
-  }
+  const filterFamily = useCallback(
+    instance => {
+      if (filters.family === 'All') {
+        return true;
+      }
+      if (filters.family === instance.family) {
+        return true;
+      }
+      return false;
+    },
+    [filters]
+  );
 
   useEffect(() => {
-    setInstances(INSTANCES)
-    // setInstances(oldInstances => oldInstances.filter(instance => filters.freeTier === instance.freeTier))
-    // setInstances(oldInstances => oldInstances.filter(instance => turbo === instance.turbo))
-    // setInstances(oldInstances => oldInstances.filter(instance => avx === instance.avx))
-    // setInstances(oldInstances => oldInstances.filter(instance => aesni === instance.aesni))
-    // setInstances(oldInstances => oldInstances.filter(instance => IPv6 === instance.IPv6))
-    // setInstances(oldInstances => oldInstances.filter(instance => EBSOptimized === instance.EBSoptimized))
-    // setInstances(oldInstances => oldInstances.filter(instance => filterFamily(instance)))
-    console.log(instances)
-    ;}, [filters])
+    setInstances(() => {
+      let allInstances = INSTANCES;
+      allInstances = allInstances.filter(instance => (filters.freeTier ? filters.freeTier === instance.freeTier : true));
+      allInstances = allInstances.filter(instance => (filters.turbo ? filters.turbo === instance.turbo : true));
+      allInstances = allInstances.filter(instance => (filters.AVX ? filters.AVX === instance.AVX : true));
+      allInstances = allInstances.filter(instance => (filters.AESNI ? filters.AESNI === instance.AESNI : true));
+      allInstances = allInstances.filter(instance => (filters.IPv6 ? filters.IPv6 === instance.IPv6 : true));
+      allInstances = allInstances.filter(instance => (filters.EBSOptimizedAvailable ? filters.EBSOptimizedAvailable === instance.EBSOptimizedAvailable : true));
+      allInstances = allInstances.filter(instance => (filters.family !== 'All' ? filterFamily(instance) : true));
+      return allInstances;
+    });
+  }, [filterFamily, filters]);
 
   return (
     <Box>
-        
       <Select value={filters.family} onChange={handleFamily} name="family">
         <MenuItem value="All">All instance families</MenuItem>
         <MenuItem value="Micro">Micro instances</MenuItem>
@@ -67,49 +71,25 @@ export default function ChooseInstanceType() {
         <MenuItem value="Memory">Memory optimized</MenuItem>
         <MenuItem value="Storage">Storage optimized</MenuItem>
       </Select>
-      
-      <FormControlLabel
-        control={
-          <Switch checked={filters.freeTier} onChange={() => handleToggle('freeTier')} size="small" value="freeTier" color="primary" />
-        }
-        label="Free Tier Only"
-      />
-      
-      <FormControlLabel
-        control={
-          <Switch checked={filters.turbo} onChange={() => handleToggle('turbo')} size="small" value="turbo" color="primary" />
-        }
-        label="Turbo"
-      />
-      
-      <FormControlLabel
-        control={
-          <Switch checked={filters.avx} onChange={() => handleToggle('avx')} size="small" value="avx" color="primary" />
-        }
-        label="AVX"
-      />
-      
-      <FormControlLabel
-        control={
-          <Switch checked={filters.aesni} onChange={() => handleToggle('aesni')} size="small" value="aesni" color="primary" />
-        }
-        label="AES-NI"
-      />
-      
-      <FormControlLabel
-        control={
-          <Switch checked={filters.IPv6} onChange={() => handleToggle('IPv6')} size="small" value="IPv6" color="primary" />
-        }
-        label="IPv6 support"
-      />
 
       <FormControlLabel
-        control={
-          <Switch checked={filters.EBSOptimized} onChange={() => handleToggle('EBSOptimized')} size="small" value="EBSOptimized" color="primary" />
-        }
+        control={<Switch checked={filters.freeTier} onChange={() => handleToggle('freeTier')} size="small" value="freeTier" color="primary" />}
+        label="Free Tier Only"
+      />
+
+      <FormControlLabel control={<Switch checked={filters.turbo} onChange={() => handleToggle('turbo')} size="small" value="turbo" color="primary" />} label="Turbo" />
+
+      <FormControlLabel control={<Switch checked={filters.avx} onChange={() => handleToggle('avx')} size="small" value="avx" color="primary" />} label="AVX" />
+
+      <FormControlLabel control={<Switch checked={filters.aesni} onChange={() => handleToggle('aesni')} size="small" value="aesni" color="primary" />} label="AES-NI" />
+
+      <FormControlLabel control={<Switch checked={filters.IPv6} onChange={() => handleToggle('IPv6')} size="small" value="IPv6" color="primary" />} label="IPv6 support" />
+
+      <FormControlLabel
+        control={<Switch checked={filters.EBSOptimized} onChange={() => handleToggle('EBSOptimized')} size="small" value="EBSOptimized" color="primary" />}
         label="EBS Optimized Available"
       />
-      
+
       <Table>
         <TableHead>
           <TableRow>
@@ -126,20 +106,20 @@ export default function ChooseInstanceType() {
           </TableRow>
         </TableHead>
         <TableBody>
-        {instances.map(instance => (
-          <TableRow>
-          <TableCell align="right">{instance.family}</TableCell>
-          <TableCell align="right">{instance.type}</TableCell>
-          <TableCell align="right">{instance.ECUs}</TableCell>
-          <TableCell align="right">{instance.vCPUs}</TableCell>
-          <TableCell align="right">{instance.physicalProcessor}</TableCell>
-          <TableCell align="right">{instance.clockSpeed} GHz</TableCell>
-          <TableCell align="right">{instance.memory}</TableCell>
-          <TableCell align="right">{instance.instanceStorage}</TableCell>
-          <TableCell align="right">{instance.networkPerformance}</TableCell>
-          <TableCell align="right">{instance.processorArchitecture}</TableCell>
-        </TableRow>
-        ))}
+          {instances.map(instance => (
+            <TableRow>
+              <TableCell align="right">{instance.family}</TableCell>
+              <TableCell align="right">{instance.type}</TableCell>
+              <TableCell align="right">{instance.ECUs}</TableCell>
+              <TableCell align="right">{instance.vCPUs}</TableCell>
+              <TableCell align="right">{instance.physicalProcessor}</TableCell>
+              <TableCell align="right">{`${instance.clockSpeed} GHz`}</TableCell>
+              <TableCell align="right">{instance.memory}</TableCell>
+              <TableCell align="right">{instance.instanceStorage}</TableCell>
+              <TableCell align="right">{instance.networkPerformance}</TableCell>
+              <TableCell align="right">{instance.processorArchitecture}</TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </Box>
