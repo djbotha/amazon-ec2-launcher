35e9ca79b1c0baef450625b92e2cd0e58b63d24e
Noah Daniel Atkins <20470797@sun.ac.za>
2019-10-18 12:05:58 +0200

    Can view modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ViewInstance from './ViewInstance';
+
+const Modal = ({ isShowing, hide, ami, expandAll }) =>
+  isShowing
+    ? ReactDOM.createPortal(
+        <React.Fragment>
+          <div className="modal-overlay" />
+          <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
+            <div className="modal">
+              <div className="modal-header">
+                <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <ViewInstance ami={ami} expandAll={expandAll}></ViewInstance>
+            </div>
+          </div>
+        </React.Fragment>,
+        document.body
+      )
+    : null;
+
+export default Modal;
