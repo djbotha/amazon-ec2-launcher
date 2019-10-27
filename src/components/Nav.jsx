93e2df628a66bbbc4f74f616f714b894af6c58ad
Daniël Botha <botha.daniel1@gmail.com>
2019-10-27 13:12:58 +0200

    Add call to launchInstance endpoint

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,7 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Badge from '@material-ui/core/Badge';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 
+import useAPI from '../hooks/useAPI';
 import { useInstance } from '../context/InstanceContext';
 import Sidebar from './Sidebar';
 
@@ -20,11 +23,35 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function DenseAppBar() {
+  const [{ data, error, loading }, launchInstanceCall] = useAPI(
+    {
+      url: '/launchInstance',
+      method: 'POST'
+    },
+    { manual: true }
+  );
+
   const classes = useStyles();
   const [open, setOpen] = useState(false);
   const { state } = useInstance();
 
   const numKeys = Object.keys(state).length;
+
+  const launchInstance = () => {
+    launchInstanceCall({
+      url: '/launchInstance',
+      data: {
+        ...state
+      }
+    });
+  };
+
+  // TODO: Remove
+  useEffect(() => {
+    console.log(data);
+    console.log(error);
+    console.log(loading);
+  }, [data, error, loading]);
 
   return (
     <div className={classes.root}>
@@ -40,9 +67,12 @@ export default function DenseAppBar() {
               <MenuIcon />
             )}
           </IconButton>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" color="inherit" className={classes.root}>
             Amazon EC2 Launch Wizard
           </Typography>
+          <Button color="inherit" endIcon={<CheckCircleIcon />} onClick={launchInstance}>
+            Launch Instance
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
